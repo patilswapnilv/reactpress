7992edd80450e77ff5d75cb189a73841bcbdc677
yann <yann.boisselier@gmail.com>
2018-10-28 18:20:51 +0100

    remove hypogriff logo

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -2,14 +2,12 @@ import Header from "../ui/Header";
 import Navigation from "../ui/Navigation";
 import Container from "../ui/Container";
 import config from "../../services/publicConfig";
-import Logo from "../../images/hippogriff.png";
 
 const Layout = ({ children }) => (
   <div>
     <Header>
       <Container>
         <h1>{config.siteInformations.name}</h1>
-        <img width="200px" src={Logo} />
         <h2>{config.siteInformations.slogan}</h2>
       </Container>
     </Header>
